6680d1affcc6eec6116c522d27b0516de0f2edf1
Durgesh Kumar Singh <93372022+Durgesh2601@users.noreply.github.com>
2022-04-29 23:23:13 +0530

    updated redux

--- my-app/src/Redux/reducers/reducer.js
+++ my-app/src/Redux/reducers/reducer.js
@@ -16,14 +16,14 @@ export const productsReducer = (state = init, {type, payload}) => {
 
 const init1 = {
     product : [],
-    reviews : []
+    reviews : [],
 }
 export const selectedProductReducer = (state=init1, {type, payload}) => {
     switch(type) {
         case SELECTED_PRODUCT : 
         return {...state, ...payload};
         case ADD_REVIEW : 
-        return {...state, reviews : {...state.reviews, payload}};
+        return {...state, reviews : [ payload]}
         case REMOVE_SELECTED_PRODUCT : 
         return {}
         default : 
